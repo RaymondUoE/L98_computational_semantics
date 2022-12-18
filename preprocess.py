1685d643131cee4e87c91b6fd00e7f83779bb15f
Raymond Wang <raymond@ITX>
2022-12-18 00:55:27 +0000

    changed preprocessing id

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,7 @@
 import os
-import pandas as pd
 import pickle
+import pandas as pd
+from tqdm import tqdm
 
 DEEPBANK_PATH = './deepbank1.1/'
 PENN_TREEBANK_PATH = './ptb/nw/wsj'
@@ -32,12 +33,13 @@ def process_semlink():
     mappings = []
     with open(SEMLINK_FILE) as f:
         file = f.readlines()
-    for line in file:
+        f.close()
+    for line in tqdm(file, total=len(file)):
         contents = line.split()
         sem_map = {}
-        sem_map['section_id'] = int(contents[0].split('/')[-2])
-        sem_map['doc_id'] = int(contents[0].split('/')[-1].split('_')[1].split('.')[0][-2:])
-        sem_map['sentence_id'] = int(contents[1])
+        # sem_map['section_id'] = int(contents[0].split('/')[-2])
+        # sem_map['doc_id'] = 
+        sem_map['id'] = '2' + str(contents[0].split('/')[-1].split('_')[1].split('.')[0][-4:]) + str(int(contents[1])+1).zfill(3)
         sem_map['token_id'] = int(contents[2])
         sem_map['is_gold'] = contents[3]
         sem_map['vb_form'] = contents[4]
@@ -52,10 +54,11 @@ def process_semlink():
         
     print('writing file to: '+ OUT_MAPPINGS_FILE)
     if IS_OUTPUT_CSV:
-        pd.DataFrame(mappings).sort_values(by=['section_id', 'doc_id', 'sentence_id']).reset_index(drop=True).to_csv(OUT_MAPPINGS_FILE, index=False)
+        pd.DataFrame(mappings).sort_values(by=['id', 'token_id']).reset_index(drop=True).to_csv(OUT_MAPPINGS_FILE, index=False)
     else:
         with open(OUT_MAPPINGS_FILE, 'wb') as handle:
             pickle.dump(mappings, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            handle.close()
         
     del mappings
         
@@ -63,55 +66,59 @@ def process_deepbank():
     print('processing deepbank')
     docs = []
 
-    for folder_name in os.listdir(DEEPBANK_PATH):
+    for folder_name in tqdm(os.listdir(DEEPBANK_PATH), total=len(os.listdir(DEEPBANK_PATH))):
         if folder_name[0] != '.':
             for file_name in os.listdir(os.path.join(DEEPBANK_PATH, folder_name)):
                 if file_name[0] != '.':
-                    # print(file_name)
                     with open(os.path.join(DEEPBANK_PATH, folder_name, file_name), 'r') as f: # open in readonly mode
                         contents = f.read().split('\n\n')
                         doc = {}
-                        doc['section_id'] = int(file_name[1:3])
-                        doc['doc_id'] = int(file_name[3:5])
-                        doc['sentence_id'] = int(file_name[5:]) - 1
+                        # doc['section_id'] = int(file_name[1:3])
+                        # doc['doc_id'] = int(file_name[3:5])
+                        # doc['sentence_id'] = int(file_name[5:]) - 1
+                        doc['id'] = contents[1].split('[', 1)[1].split(']', 1)[0]
                         doc['sentence'] = contents[1].split('`')[1].split('\'')[0]
                         doc['constituency'] = contents[5]
                         doc['eds'] = contents[7]
                         doc['dependency'] = contents[8]
 
                         docs.append(doc)
+                        f.close()
     
     print('writing file to: '+ OUT_SENTENCES_FILE)
     if IS_OUTPUT_CSV:
-        pd.DataFrame(docs).sort_values(by=['section_id', 'doc_id', 'sentence_id']).reset_index(drop=True).to_csv(OUT_SENTENCES_FILE, index=False)
+        pd.DataFrame(docs).sort_values(by=['id']).reset_index(drop=True).to_csv(OUT_SENTENCES_FILE, index=False)
     else:
         with open(OUT_SENTENCES_FILE, 'wb') as handle:
             pickle.dump(docs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            handle.close()
     del docs
             
 def process_trees():
     print('processing trees')
     trees = []
 
-    for section_id in os.listdir(PENN_TREEBANK_PATH):
+    for section_id in tqdm(os.listdir(PENN_TREEBANK_PATH), total=len(os.listdir(PENN_TREEBANK_PATH))):
         for file_name in os.listdir(os.path.join(PENN_TREEBANK_PATH, section_id)):
             with open(os.path.join(PENN_TREEBANK_PATH, section_id, file_name), 'r') as f: # open in readonly mode
                 contents = f.read().split('\n\n')
                 for t, i in zip(contents, range(len(contents))):
                     my_dict = {}
-                    my_dict['section_id'] = int(section_id)
-                    my_dict['doc_id'] = int(file_name.split('.')[0][-2:])
-                    my_dict['sentence_id'] = int(i)
+                    # my_dict['section_id'] = int(section_id)
+                    my_dict['id'] = '2' + str(file_name.split('.')[0][-4:]) + str(int(i) + 1).zfill(3)
+                    # my_dict['sentence_id'] = 
                     my_dict['tree'] = t
 
                     trees.append(my_dict)
+                f.close()
                     
     print('writing file to: '+ OUT_TREE_FILE)
     if IS_OUTPUT_CSV:
-        pd.DataFrame(trees).sort_values(by=['section_id', 'doc_id', 'sentence_id']).reset_index(drop=True).to_csv(OUT_TREE_FILE, index=False)    
+        pd.DataFrame(trees).sort_values(by=['id']).reset_index(drop=True).to_csv(OUT_TREE_FILE, index=False)    
     else:
         with open(OUT_TREE_FILE, 'wb') as handle:
             pickle.dump(trees, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            handle.close()
     del trees
             
 if __name__ == "__main__":
